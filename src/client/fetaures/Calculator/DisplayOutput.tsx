0dd33fd0a3128aa35a275501669b20a08c56d605
Christian Todd <Christian.Todd7@gmail.com>
2020-02-08 22:32:41 -0500

    remove more animations.

--- src/client/fetaures/Calculator/DisplayOutput.tsx
+++ src/client/fetaures/Calculator/DisplayOutput.tsx
@@ -22,45 +22,34 @@ let TimeResult = styled("div")`
   width: 100%;
 `
 
+let StyledNum = styled("div")`
+  font-size: 5.2rem;
+  font-variant-numeric: tabular-nums;
+  display: inline-block;
+  line-height: 1.4;
+  @media (min-width: 720px) {
+    line-height: auto;
+  }
+`
+
 function DisplayOutput({ unit, length, width, height }: State) {
-  let [animNumbers, setAnimNumbers] = React.useState(true)
   let result = React.useMemo(() => {
     return unit === "mm"
       ? meanSacDiameter(length, width, height)
       : meanSacDiameter(cmToMm(length), cmToMm(width), cmToMm(height))
   }, [unit, length, width, height])
 
-  /**
-   * We only want the trailing reveal on the first mount,
-   * updates to the result should not have their position/opacity
-   * animated.
-   *
-   * TODO: Find better way of determining when the transition has ended from here
-   */
-  React.useEffect(() => {
-    let handle = setTimeout(() => {
-      setAnimNumbers(false)
-    }, 2000)
-    return () => {
-      if (handle) {
-        clearInterval(handle)
-      }
-    }
-  }, [])
-
   result = useSpring(result)
 
   return (
     <ResultContainer>
       <TimeResult>
-        <StyledNum animNumbers={animNumbers}>{`${result | 0}`}</StyledNum>
+        <StyledNum>{`${result | 0}`}</StyledNum>
         <TimeLabel>Days</TimeLabel>
       </TimeResult>
 
       <TimeResult>
-        <StyledNum animNumbers={animNumbers}>
-          {daysToWeeks(result).toFixed(2)}
-        </StyledNum>
+        <StyledNum>{daysToWeeks(result).toFixed(2)}</StyledNum>
         <TimeLabel>Weeks</TimeLabel>
       </TimeResult>
     </ResultContainer>
@@ -77,65 +66,5 @@ let TimeLabelSpan = styled("span")`
 let TimeLabel = React.memo(function TimeLabel({ children }) {
   return <TimeLabelSpan>{children}</TimeLabelSpan>
 })
-
-function StyledNum({
-  children,
-  animNumbers
-}: {
-  children: string
-  animNumbers: boolean
-}) {
-  let chars = React.useMemo(() => children.split(""), [children])
-
-  return (
-    <div>
-      {chars.map((ch, i) => {
-        return (
-          <StyledChar
-            animNumbers={animNumbers}
-            char={ch}
-            offset={i + 1}
-            key={`${i}${ch}`}
-          />
-        )
-      })}
-    </div>
-  )
-}
-
-let StyledCharSpan = styled("span")`
-  font-size: 5.2rem;
-  font-variant-numeric: tabular-nums;
-  display: inline-block;
-  line-height: 1.4;
-  @media (min-width: 720px) {
-    line-height: auto;
-  }
-`
-
-function StyledChar({ char, offset, animNumbers }) {
-  const [trans, setTrans] = React.useState(8 * (offset * 1.5))
-  const [opacity, setOpacity] = React.useState(0)
-  const sprungTrans = useSpring(trans)
-  const sprungOp = useSpring(opacity)
-
-  React.useEffect(() => {
-    if (animNumbers) {
-      setTrans(0)
-      setOpacity(1)
-    }
-  }, [animNumbers])
-
-  return (
-    <StyledCharSpan
-      style={{
-        transform: `translateY(${animNumbers ? sprungTrans : 0}px)`,
-        opacity: animNumbers ? sprungOp : 1
-      }}
-    >
-      {char}
-    </StyledCharSpan>
-  )
-}
 
 export { DisplayOutput }
